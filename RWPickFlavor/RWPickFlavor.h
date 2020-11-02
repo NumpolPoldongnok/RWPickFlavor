0ee2b14f4c3a0bfa0cfa693aa920a834f340fe4f
Numpol Poldongnok <numpol.pol@ascendcorp.com>
2020-11-02 20:55:34 +0700

    Initial commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Numpol Poldongnok on 2/11/2563 BE.
+//  Copyright © 2563 Numpol Poldongnok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
